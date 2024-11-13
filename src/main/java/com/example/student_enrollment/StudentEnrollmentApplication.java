031650593450cfa72e449f29e2836c1291bf618c
zbflores <zbflores@iu.edu>
2024-11-13 02:49:13 -0500

    Initial commit: Project setup

---
+++ src/main/java/com/example/student_enrollment/StudentEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrollmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_enrollment/StudentEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
